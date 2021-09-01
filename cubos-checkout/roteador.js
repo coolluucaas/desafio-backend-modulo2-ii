fe258b8412c5743bf878d86d85cc70685d905c44
coolluucaas <lucaspsanta@gmail.com>
2021-09-01 15:13:45 -0300

    add rotas para 'criarPedido' e 'consultarPedido'

--- cubos-checkout/roteador.js
+++ cubos-checkout/roteador.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { listarProdutos, detalharCarrinho, adicionarProdutoAoCarrinho, modificarProdutoDoCarrinho, deletarProdutoDoCarrinho, limparCarrinho, finalizarCompra } = require("./controladores/controladores");
+const { criarPedido, consultarPedido } = require("./controladores/pedidos");
 
 const roteador = express();
 
@@ -10,5 +11,7 @@ roteador.patch("/carrinho/produtos/:idProduto", modificarProdutoDoCarrinho);
 roteador.delete("/carrinho/produtos/:idProduto", deletarProdutoDoCarrinho);
 roteador.delete("/carrinho", limparCarrinho);
 roteador.post("/carrinho/finalizar-compra", finalizarCompra);
+roteador.post("/carrinho/criar-pedido", criarPedido);
+roteador.post("/carrinho/consultar-pedido/:id", consultarPedido);
 
 module.exports = roteador;
