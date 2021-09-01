c75e027ea0fb197db94e2ff31dd1eb33cc418181
coolluucaas <lucaspsanta@gmail.com>
2021-09-01 10:22:55 -0300

    modificacao

--- exemploLerDoArquivo.js
+++ exemploLerDoArquivo.js
@@ -1,8 +1,16 @@
+const fs = require("fs/promises");
+
 // importamos a função lerArquivo do arquivo bibliotecaFS
 const { lerArquivo } = require('./bibliotecaFS');
 
-
 // Lendo o que está dentro do arquivo (data.json) 
 // depois de ler, dentro da varíavel dadosDoArquivo tem todas as informações do arquivo data.json.
-const dadosDoArquivo = await lerArquivo();
 
+
+const lerArquivoJson = async () => {
+    await lerArquivo().then((data) => {
+        console.log("Terminei de ler:", data.toStringfy());
+    });
+}
+
+lerArquivoJson();
